2537d52a12546090d461f2d98b59641dbe7ff99a
Liam Deacon <liam.m.deacon@gmail.com>
2017-07-04 21:44:00 +0100

    More group changes

---
+++ server/api/group/group.events.js
@@ -0,0 +1,35 @@
+/**
+ * Thing model events
+ */
+
+'use strict';
+
+import {EventEmitter} from 'events';
+var GroupEvents = new EventEmitter();
+
+// Set max event listeners (0 == unlimited)
+GroupEvents.setMaxListeners(0);
+
+// Model events
+var events = {
+  save: 'save',
+  remove: 'remove'
+};
+
+// Register the event emitter to the model events
+function registerEvents(Group) {
+  for(var e in events) {
+    let event = events[e];
+    Group.post(e, emitEvent(event));
+  }
+}
+
+function emitEvent(event) {
+  return function(doc) {
+    ThingEvents.emit(`${event}:${doc._id}`, doc);
+    ThingEvents.emit(event, doc);
+  };
+}
+
+export {registerEvents};
+export default GroupEvents;
